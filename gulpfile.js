50d9ae77a879475979e98a064048f1b37353ad9f
Nadia Pillard <ua9ond@yandex.ru>
2016-05-25 16:39:15 +0600

    gulpfile done

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var copy = require("gulp-contrib-copy");
+var clean = require('gulp-contrib-clean');
 //var jade = require("gulp-jade");
-//var uglify = require("gulp-uglify");
+var uglify = require("gulp-uglify");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var imagemin = require("gulp-imagemin");
@@ -29,34 +31,53 @@ gulp.task("style", function(){
     
   ]))
   
-  .pipe(gulp.dest("css"))
+  .pipe(gulp.dest("build/css"))
   .pipe(csso())
   .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("css"))
+  .pipe(gulp.dest("build/css"))
   .pipe(server.reload({stream: true}));
+});
+
+gulp.task("minjs", function(){
+  gulp.src("js/**/*.js")
+  .pipe(gulp.dest("build/js/"))
+  .pipe(uglify())
+  .pipe(rename("main.min.js"))
+  .pipe(gulp.dest("build/js/"))
+  
 });
 
 gulp.task("image", function(){
   return gulp.src("img/**/*.{png,jpg,gif}")
-  
   .pipe(imagemin({
     optimizationLevel: 3,
     progressive: true 
   }))
-  .pipe(gulp.dest("img"))
+  .pipe(gulp.dest("build/img"))
 });
 
-gulp.task("show", ["style"], function(){
+gulp.task("clean", function () {
+  return gulp.src("build", {read: false})
+    .pipe(clean());
+});
+
+gulp.task("copy", function() {
+  gulp.src("*.html")
+  .pipe(copy())
+  .pipe(gulp.dest("build"))
+})
+
+gulp.task("show", function(){
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     ui: false
   });
   
   gulp.watch("less/**/*.less", ["style"]);
-   gulp.watch("*.html")     
-     .on("change", server.reload);
-
-  
+  gulp.watch("*.html", ["copy"]).on("change", server.reload);
+  gulp.watch("js/*js", ["minjs"]).on("change", server.reload);
 });
+
+gulp.task("build", ["clean", "copy", "style", "minjs", "image"]);
