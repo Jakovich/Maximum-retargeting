cc39e3307c5d336e4165b2d1bff481a18ef1dbb8
Nadia Pillard <ua9ond@yandex.ru>
2016-05-27 19:29:12 +0600

    slider size changed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 "use strict";
 
 $(document).ready(function() {
- 
-$("#up-scroll").click(function() {
+  
+  
+  //скролл наверх страницы при нажатии кнопки 
+  $("#up-scroll").click(function() {
 
     $("body,html").animate({scrollTop:0},800);
 
   });
+  
+  //настройка сладера
+  $('.carousel').carousel({
+ 
+    directionNav:false,
+    shadow:false,
+    buttonNav:'bullets',
+    autoplay: false,
+    hMargin:0.1,
+    frontWidth: 800,
+    frontHeight: 535,
+    carouselWidth: 900,
+    carouselHeight: 595,
+    backOpacity: 0.8
+  
   });
+  
+});
--- js/vendor/sample01.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){$('.carousel').carousel({
-  carouselWidth:930,
-  carouselHeight:330,
-  directionNav:false,
-  shadow:false,
-  buttonNav:'bullets',
-  autoplay: false,
-  hMargin:0.1,});});
