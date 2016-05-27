8a4c68860cc89ddd72291b08ae4aeb930aefe185
Nadia Pillard <ua9ond@yandex.ru>
2016-05-27 18:31:32 +0600

    video slider added

---
+++ js/vendor/sample01.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){$('.carousel').carousel({
+  carouselWidth:930,
+  carouselHeight:330,
+  directionNav:false,
+  shadow:false,
+  buttonNav:'bullets',
+  autoplay: false,
+  hMargin:0.1,});});
